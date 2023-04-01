e6503b662d344de7b1a51af2610eaf5771e18d8e
Omid Khodaparast <guskhodom@student.gu.se>
2023-04-01 22:25:40 +0200

    Commit fix
    In the last commit of this branch, imissed to push some of the changes. Since I noticed it now. I am pushing it now

---
+++ SonarVisualizer/src/components/MqttHandler.svelte
@@ -0,0 +1,12 @@
+<script context="module">
+import * as mqtt from "mqtt"  // import everything inside the mqtt module and give it the namespace "mqtt"
+import { Buffer } from "buffer";
+
+export async function connectMqtt(){
+    let client = mqtt.connect('mqtt://test.mosquitto.org') // create a client
+
+
+}
+
+
+</script>
---
+++ SonarVisualizer/src/components/Title.svelte
@@ -0,0 +1,34 @@
+<script>
+import { connectMqtt } from "./MqttHandler.svelte";
+var animals=["monkey","fish","donkey","banana"]
+var randomAnimal=animals[1]
+setTimeout(async function(){
+    randomAnimal="bob";
+},5000)
+
+
+for (let i = 0; i < 25; i++) {
+    console.log("bob");
+}
+let x=10;
+while(x>0){
+    x--;
+}
+console.warn("hello");
+console.error("hello");
+if(true==true){
+    console.log("test");
+}
+
+connectMqtt();
+</script>
+
+<h1>I am a {randomAnimal}</h1>
+
+<style>
+
+    h1{
+        color :red;
+        font-size: 24px;
+    }
+</style>
