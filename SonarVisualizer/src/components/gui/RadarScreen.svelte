0c869eedca06fb57d024fac0f0416549ec51ad39
janos <janos@student.chalmers.se>
2023-04-06 12:31:09 +0200

    #22 Draw radar screen

--- SonarVisualizer/src/components/gui/RadarScreen.svelte
+++ SonarVisualizer/src/components/gui/RadarScreen.svelte
@@ -12,38 +12,89 @@
     Used for communicating data from Mqtt component and radar screen
     */
     
-    let deg;
-    let dist;
-    let canvasEl;
     const screenRadius = 200;
     const canvas = {
         width: screenRadius*2,
         height: screenRadius*2
     };
-    let context;
+
+    let context; 
+    let deg;
+    let dist;
+    let range;
+    let canvasEl;
     
     sonarStore.subscribe(sonarStore => {
         deg = sonarStore.sonarData.deg;
         dist = sonarStore.sonarData.dist;
+        // Set it to maximum range for now (in cm)
+        range = 350;
     });
 
-    const drawCircle = (context) => {
+    const drawRadar = (context) => {
         context.beginPath();
-        context.arc(screenRadius, screenRadius, screenRadius, 0, 2 * Math.PI);
+        context.strokeStyle = 'grey';
+        context.lineWidth = 1;
+        context.translate(screenRadius, screenRadius);
+        // Draw concentric circles
+        context.arc(0, 0, screenRadius, 0, 2 * Math.PI);
+        context.moveTo(screenRadius * 3/4, 0);
+        context.arc(0, 0, screenRadius * 3/4, 0, 2 * Math.PI);
+        context.moveTo(screenRadius * 2/4, 0);
+        context.arc(0, 0, screenRadius * 2/4, 0, 2 * Math.PI);
+        context.moveTo(screenRadius * 1/4, 0);
+        context.arc(0, 0, screenRadius * 1/4, 0, 2 * Math.PI);
         context.stroke();
+        // Draw lines dividing sectors
+        context.moveTo(0, 0);
+        context.lineTo(Math.cos(toRadians(30)) * - screenRadius, Math.sin(toRadians(30)) * - screenRadius);
+        context.moveTo(0, 0);
+        context.lineTo(Math.cos(toRadians(60)) * - screenRadius, Math.sin(toRadians(60)) * - screenRadius);
+        context.moveTo(0, 0);
+        context.lineTo(Math.cos(toRadians(90)) * - screenRadius, Math.sin(toRadians(90)) * - screenRadius);
+        context.moveTo(0, 0);
+        context.lineTo(Math.cos(toRadians(120)) * - screenRadius, Math.sin(toRadians(120)) * - screenRadius);
+        context.moveTo(0, 0);
+        context.lineTo(Math.cos(toRadians(150)) * - screenRadius, Math.sin(toRadians(150)) * - screenRadius);
+        context.moveTo(0, 0);
+        context.lineTo(Math.cos(toRadians(180)) * - screenRadius, Math.sin(toRadians(180)) * - screenRadius);
+        context.moveTo(0, 0);
+        context.lineTo(Math.cos(toRadians(30)) * screenRadius, Math.sin(toRadians(30)) * screenRadius);
+        context.moveTo(0, 0);
+        context.lineTo(Math.cos(toRadians(60)) * screenRadius, Math.sin(toRadians(60)) * screenRadius);
+        context.moveTo(0, 0);
+        context.lineTo(Math.cos(toRadians(90)) * screenRadius, Math.sin(toRadians(90)) * screenRadius);
+        context.moveTo(0, 0);
+        context.lineTo(Math.cos(toRadians(120)) * screenRadius, Math.sin(toRadians(120)) * screenRadius);
+        context.moveTo(0, 0);
+        context.lineTo(Math.cos(toRadians(150)) * screenRadius, Math.sin(toRadians(150)) * screenRadius);
+        context.moveTo(0, 0);
+        context.lineTo(Math.cos(toRadians(180)) * screenRadius, Math.sin(toRadians(180)) * screenRadius);
+        context.stroke();
+
     }
 
     const drawLineTo = (context, x, y) => {
-        context.strokeStyle = 'white';
-        context.lineWidth = 2;
+        context.beginPath();
+        context.strokeStyle = 'green';
+        context.lineWidth = 3;
         context.moveTo(canvas.width/2, canvas.height/2);
         context.lineTo(x, y);
         context.stroke();
     };
 
     const getCoordinates = (deg, dist) => {
-        let mappedDist = mapDistance(dist);
-        // TODO
+        let mappedDist = mapDistance(dist, range);
+        // X = cos(deg) * dist
+        // Y = sin(deg) * dist
+        return {
+            x: Math.cos(toRadians(deg)) * mappedDist,
+            y: Math.sin(toRadians(deg)) * mappedDist
+        };
+    }
+
+    const toRadians = (deg) => {
+        return deg * (Math.PI / 180);
     }
     
     /**
@@ -59,8 +110,7 @@
     onMount(() => {
         console.log("RadarScreen mounted");
         context = canvasEl.getContext('2d');
-        drawCircle(context);
-        drawLineTo(context, 400, 200);
+        drawRadar(context);
     });
     
 </script>
