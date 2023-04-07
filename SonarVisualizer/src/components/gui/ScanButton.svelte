536e7d9822a56f9cb5aa414f203f01fea5db9ec2
Amirpooya <amiasa@student.chalmers.se>
2023-04-07 15:52:30 +0200

    #16 Stop and start the sonar
     Impelements button for controlling sonar for now it just contains the gui, mqtt will be added later.

---
+++ SonarVisualizer/src/components/gui/ScanButton.svelte
@@ -0,0 +1,64 @@
+<script>
+    'use strict';
+
+    import { createEventDispatcher } from 'svelte';
+                
+    let scanning = false; // SCANNING -> status: true   NOT SCANNING -> status: false (by default).       
+
+    const dispatch = createEventDispatcher();
+
+    function start() {
+        //TODO: probably this method will need some updates for mqtt protocol
+        scanning = true;
+
+        dispatch('start',{
+            scanning: true
+        });
+        
+    }
+
+    function stop() {
+        scanning = false;
+
+        dispatch('stop',{
+            scanning: false
+        });
+    }
+
+</script>
+
+<main>
+    {#if scanning === false}
+        <button class='my-button' on:click= {start}>START  &nbsp;SCANNING</button>
+    {:else}
+        <button class='my-button' on:click={stop}>STOP &nbsp;SCANNING</button>
+    {/if}
+</main>
+
+<style>
+    :global(*){
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box; 
+    }
+
+    .my-button{
+        color: #F5F5F5;
+        background-color: #333333;
+        width: 20em;
+        height: 7em;
+        border-radius: 10px;
+        border: none;
+        text-align: center;
+        font-size: 1em;
+        font-family: 'IBM Plex Sans';
+        font-weight: bold;
+        transition: all 0.3s ease;
+    }
+
+    .my-button:hover{
+        opacity: 0.8;
+        box-shadow: 0px 0px 10px rgba(255, 255, 255, 0.5);
+    }
+
+</style>
