5af0b048e56cdb659d46e34c2f2c85c32931e7a6
alesaf <gussafala@student.gu.se>
2023-04-14 19:52:30 +0200

    #28 Added mqtt data simulation

--- SonarVisualizer/src/data/stores.js
+++ SonarVisualizer/src/data/stores.js
@@ -3,8 +3,10 @@ import { writable } from "svelte/store";
 export let sonarStore=writable({
     //Data for sonar as per agreed description
     "sonarData":{
-        "deg": "125", //Degree of the sonar
-        "dist": "24.56", //Distance of sonar
+        "deg1": "125", //Degree of the sonar
+        "dist1": "24.56", //Distance of sonar
+        "deg2": "125", //Degree of the sonar
+        "dist2": "24.56", //Distance of sonar
         "range": "350",
         "ts":0 //Reported at timestamp
     },
