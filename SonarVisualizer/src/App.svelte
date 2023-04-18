859020c48b55ed189b9eddf435c45604dd53637b
arumeel <kaisa.arumeel@gmail.com>
2023-04-18 17:35:56 +0200

    KiWave logo
    Added KiWave logo to the page, also changed the browser title bar.

--- SonarVisualizer/src/App.svelte
+++ SonarVisualizer/src/App.svelte
@@ -3,16 +3,23 @@
   
    
 import Sonar from './components/Sonar.svelte';
-    import MqttHandler from './components/mqtt/MqttHandler.svelte';
+import MqttHandler from './components/mqtt/MqttHandler.svelte';
 
   </script>
   
   <main>
+
+      <img src="KiWave.png" class="kiwave-logo" alt="KiWave logo">
+    
     <MqttHandler/>
     <Sonar/>
+    
   </main>
   
   <style>
-
+    .kiwave-logo{
+      width: 300px;
+      height: 200px;
+    }
   </style>
   
