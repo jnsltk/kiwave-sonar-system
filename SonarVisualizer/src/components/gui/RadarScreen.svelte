c6ede0e4b4ba337850f9a42b2213344fb673cc20
janos <janos@student.chalmers.se>
2023-04-19 12:21:17 +0200

    #38 Remove test from RadarScreen

--- SonarVisualizer/src/components/gui/RadarScreen.svelte
+++ SonarVisualizer/src/components/gui/RadarScreen.svelte
@@ -237,30 +237,6 @@
         draw(deg2, dist2);
     }
 
-    //TESTING - Simulating change of sonarData
-    async function sonarSim(){
-        async function getRandomInt(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-
-        return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
-        }
-        // for (let i = 0; i < 360; i++) {
-        //     setTimeout(async function() {
-        //         $sonarStore.sonarData.deg=i.toString()
-        //         $sonarStore.sonarData.dist=(await getRandomInt(0,350)).toString()
-        //     }, 30*i)
-        // }
-        setInterval(async function(){
-            let deg = (await getRandomInt(0,360))
-            $sonarStore.sonarData.rDeg1=(deg).toString()
-            $sonarStore.sonarData.rRange1=(await getRandomInt(0,range)).toString()
-            $sonarStore.sonarData.rDeg2=(deg + 180).toString()
-            $sonarStore.sonarData.rRange2=(await getRandomInt(0,range)).toString()
-        },1000)
-    }
-    sonarSim()
-
     onMount(() => {
         console.log("RadarScreen mounted");
         resetScreen();
