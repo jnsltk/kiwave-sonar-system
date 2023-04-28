80667f412bab763c8f6c20a97c27c11b2849607e
jnsltk <janos.litkei@gmail.com>
2023-04-28 11:59:48 +0200

    #53 Fix RadarScreen, display currently measured sectors

--- SonarVisualizer/src/components/gui/RadarScreen/Object.svelte
+++ SonarVisualizer/src/components/gui/RadarScreen/Object.svelte
@@ -10,8 +10,10 @@
     $: deg1 = parseInt($sonarStore.sonarData.rDeg1);
     $: deg2 = parseInt($sonarStore.sonarData.rDeg2);
     $: range = parseInt($sonarCommands.sonarData.sRange);
+    let history = [];
 
     const objectColor = "#585858";
+    const activeSectorColor = "#007AFF"
     const lineToObjectColor = "#D5D7D4";
         
     /**
@@ -21,9 +23,9 @@
      * @param deg The angle at which the object was detected
      * @param dist The distance from the sensor
      */
-    const drawLineTo = (context, deg, dist, width, height) => {
+    const drawLineTo = (context, deg, dist, width, height, currentlyActive) => {
         for (let i = parseInt(deg) - 7; i < parseInt(deg) + 8; i++) {
-            const endCoordinates = getCoordinates(i, dist);
+            const endCoordinates = getCoordinates(i, dist, $sonarCommands.sonarData.sRange, screenRadius);
             context.save();
             context.translate(width / 2, height / 2);
             context.beginPath();
@@ -34,6 +36,19 @@
             context.stroke();
             context.restore();
         };
+        // Draws the blue line last so it's not drawn on and covered
+        if (currentlyActive) {
+            const endCoordinates = getCoordinates(deg, dist, $sonarCommands.sonarData.sRange, screenRadius);
+            context.save();
+            context.translate(width / 2, height / 2);
+            context.beginPath();
+            context.strokeStyle = activeSectorColor;
+            context.lineWidth = 3;
+            context.moveTo(0, 0);
+            context.lineTo(endCoordinates.x, endCoordinates.y);
+            context.stroke();
+            context.restore();
+        }
     };
 
 
@@ -44,7 +59,7 @@
      * @param deg The angle at which the object was detected
      * @param dist The distance from the sensor
      */
-    const drawObjectFrom = (context, deg, dist, width, height) => {
+    const drawObjectFrom = (context, deg, dist, width, height, currentlyActive) => {
         // Calculate coordinates for the endpoint for the line on the circle
         for (let i = parseInt(deg) - 7; i < parseInt(deg) + 8; i++) {
             const startCoordinates = getCoordinates(i, dist, $sonarCommands.sonarData.sRange, screenRadius);
@@ -59,17 +74,48 @@
             context.stroke();
             context.restore();
         }
+        // Draws the blue line last so it's not drawn on and covered
+        if (currentlyActive) {
+            const startCoordinates = getCoordinates(deg, dist, $sonarCommands.sonarData.sRange, screenRadius);
+            const endCoordinates = getCoordinates(deg, $sonarCommands.sonarData.sRange, $sonarCommands.sonarData.sRange, screenRadius);
+            context.save();
+            context.translate(width / 2, height / 2);
+            context.beginPath();
+            context.strokeStyle = activeSectorColor;
+            context.lineWidth = 3;
+            context.moveTo(startCoordinates.x, startCoordinates.y);
+            context.lineTo(endCoordinates.x, endCoordinates.y);
+            context.stroke();
+            context.restore();
+        }
     };
 
 
     $: render = ({ context, width, height }) => {
+        history.forEach(element => {
+            if (element.dist1 > range) element.dist1 = range;
+            drawObjectFrom(context, element.deg1, element.dist1, width, height, false);
+            drawLineTo(context, element.deg1, element.dist1, width, height, false);
+
+            if (element.dist2 > range) element.dist2 = range;
+            drawObjectFrom(context, element.deg2, element.dist2, width, height, false);
+            drawLineTo(context, element.deg2, element.dist2, width, height, false);
+        });
+
         if (dist1 > range) dist1 = range;
-        drawObjectFrom(context, deg1, dist1, width, height);
-        drawLineTo(context, deg1, dist1, width, height);
+        drawObjectFrom(context, deg1, dist1, width, height, true);
+        drawLineTo(context, deg1, dist1, width, height, true);
 
         if (dist2 > range) dist2 = range;
-        drawObjectFrom(context, deg2, dist2, width, height);
-        drawLineTo(context, deg2, dist2, width, height);
+        drawObjectFrom(context, deg2, dist2, width, height, true);
+        drawLineTo(context, deg2, dist2, width, height, true);
+
+        history.push({
+            "deg1": deg1,
+            "deg2": deg2,
+            "dist1": dist1,
+            "dist2": dist2
+        })
     }
 </script>
 
