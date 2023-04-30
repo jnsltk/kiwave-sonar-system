f63d973421bf5fc7366eae520e6dd77c00c04f6d
0R61 <73043021+0R61@users.noreply.github.com>
2023-04-30 02:04:17 +0200

    added keep alive

--- SonarController/KiwiMQTT.cpp
+++ SonarController/KiwiMQTT.cpp
@@ -9,7 +9,7 @@ KiwiMQTT::KiwiMQTT(char* ssid,char* secret) {
     _secret=secret; 
     _wifiClient=WiFiClient();
     _pubSubClient=PubSubClient(_wifiClient);
-    //Modi
+    //Modifiying keep alive to prevent connection drop
     _pubSubClient.setSocketTimeout(20);
     _pubSubClient.setKeepAlive(20);
  }
