7e5c93828401da0c4e6fa3e35121b758f2270276
arumeel <kaisa.arumeel@gmail.com>
2023-05-02 10:37:54 +0200

    Connect loading scene to isOnline boolean flag

--- SonarVisualizer/src/App.svelte
+++ SonarVisualizer/src/App.svelte
@@ -1,31 +1,51 @@
 <script>
   // @ts-nocheck
   
-   
+  
 import Sonar from './components/Sonar.svelte';
 import MqttHandler from './components/mqtt/MqttHandler.svelte';
 import ModeSwitch from './components/gui/ModeSwitch.svelte';
+import LoadingScene from './components/gui/LoadingScene.svelte';
+import {sonarStore} from "./data/stores";
 
-  </script>
-  
-  <main> 
-       
-    <MqttHandler/>
+const isSonarOnline = $sonarStore.sonarStatus.isOnline;
+
+</script>
+
+<main> 
+   <MqttHandler/>
+  {#if isSonarOnline}
     <Sonar/>
-    <div class="modeswitch">
-      <ModeSwitch/>  
+  {:else}
+    <div class="loader">
+      <LoadingScene/>
     </div>
-    
+  {/if}
 
-  </main>
-  
-  <style>
+ 
+  <div class="modeswitch">
+    <ModeSwitch/>  
+  </div>
+</main>
 
-.modeswitch {
-        position: absolute;
-        top: 0.3rem;
-        right: 2rem;
+<style>
 
-    }
-  </style>
-  
+  .modeswitch {
+      position: absolute;
+      top: 0.3rem;
+      right: 2rem;
+
+  }
+
+    .loader {
+    position: fixed;
+    background-color: #eef1ec;
+    height: 100%;
+    width: 100%;
+    z-index: 10000;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+</style>
