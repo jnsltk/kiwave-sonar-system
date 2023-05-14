76b1df5491496b186d3b80c104cafce09e199783
arumeel <kaisa.arumeel@gmail.com>
2023-05-14 09:57:25 +0200

    Removed unnecessary media query

--- SonarVisualizer/src/components/gui/NotificationButton.svelte
+++ SonarVisualizer/src/components/gui/NotificationButton.svelte
@@ -30,12 +30,6 @@ function toggleNotification() {
       box-shadow: rgba(255, 255, 255, 0.1) 0px 1px 1px 0px inset, rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
     }
   
-    @media screen and (max-width:600px) {
-      .notification-button {
-        width:90%; 
-        margin-bottom: 50px;  
-      }
-    }
   
     .notification-button:hover {
       opacity: 0.8;
